f09a444c2076282b3a2e9751e7d2476b5155d59d
j-baldock <jbaldock@usgs.gov>
2025-09-25 14:11:22 -0600

    initial render to quarto

--- build_book.R
+++ build_book.R
@@ -5,5 +5,4 @@ quarto::quarto_render(output_format = "html")
 
 
 quarto::quarto_render("index.qmd", output_format = "html")
-quarto::quarto_render("SnakeModelResid.qmd", output_format = "html")
-quarto::quarto_render("pasta_gw.qmd", output_format = "html")
+
