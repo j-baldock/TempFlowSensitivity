5054a19d26d707e241424569a030758b18fd4a54
Jeff Baldock <j.baldock1991@gmail.com>
2025-10-02 16:15:38 -0600

    update

--- build_book.R
+++ build_book.R
@@ -1,10 +1,11 @@
 library(quarto)
 library(knitr)
 
+# render whole book
 quarto::quarto_render(output_format = "html")
 
-
+# render single chapters
 quarto::quarto_render("index.qmd", output_format = "html")
-quarto::quarto_render("FormatData.qmd", output_format = "html")
+quarto::quarto_render("DataViz.qmd", output_format = "html")
 quarto::quarto_render("Breakpoints.qmd", output_format = "html")
-
+quarto::quarto_render("FormatData.qmd", output_format = "html")
