4c8235e61931b755eb750d16efb604b0fea45a9f
Jeff Baldock <j.baldock1991@gmail.com>
2025-10-02 11:51:05 -0600

    update format data

--- build_book.R
+++ build_book.R
@@ -6,4 +6,5 @@ quarto::quarto_render(output_format = "html")
 
 quarto::quarto_render("index.qmd", output_format = "html")
 quarto::quarto_render("FormatData.qmd", output_format = "html")
+quarto::quarto_render("Breakpoints.qmd", output_format = "html")
 
